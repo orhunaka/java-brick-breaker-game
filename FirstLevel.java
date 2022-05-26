b0c432cdf94a873c8d77576158c9b0288e16f24b
Orhun Aka <orhunka@outlook.com>
2022-05-26 13:22:43 +0300

    Added player icon

--- FirstLevel.java
+++ FirstLevel.java
@@ -14,15 +14,25 @@ public class FirstLevel extends JFrame {
         super("Level 1");
         setLayout(null);
 
-        ImageIcon icon = new ImageIcon("ball.png");
-        Image img = icon.getImage();
-        Image scaledImg = img.getScaledInstance(30, 30, Image.SCALE_SMOOTH);
-        ImageIcon scaledIcon = new ImageIcon(scaledImg);
+        ImageIcon ballIcon = new ImageIcon("ball.png");
+        Image ballImg = ballIcon.getImage();
+        Image scaledBallImg = ballImg.getScaledInstance(30, 30, Image.SCALE_SMOOTH);
+        ImageIcon scaledBallIcon = new ImageIcon(scaledBallImg);
         ball = new JLabel();
-        ball.setIcon(scaledIcon);
+        ball.setIcon(scaledBallIcon);
+
+        ImageIcon playerIcon = new ImageIcon("player.png");
+        Image playerImg = playerIcon.getImage();
+        Image scaledPlayerImg = playerImg.getScaledInstance(80, 20, Image.SCALE_SMOOTH);
+        ImageIcon scaledPlayerIcon = new ImageIcon(scaledPlayerImg);
+        player = new JLabel();
+        player.setIcon(scaledPlayerIcon);
 
         add(ball);
         ball.setBounds(100, 100, 30, 30);
+
+        add(player);
+        player.setBounds(180, 240, 80, 20);
         
     }
 }
